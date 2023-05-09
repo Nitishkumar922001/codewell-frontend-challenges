cdc2e6ebfeb624908b93593f23eb0a9813e05f12
Nitishkumar922001 <miranamnitish@gmail.com>
2023-05-09 13:23:33 +0530

    main-section-done

--- indiebrew-page/src/App.js
+++ indiebrew-page/src/App.js
@@ -1,15 +1,21 @@
 
 import './App.css';
+import BodySection from './componets/body-section/BodySection';
 import HeadSection from './componets/head-section/HeadSection';
 import Nav from './componets/nav/Nav';
 
 function App() {
   return (
     <div className='page-container'>
-    <header className='header'>
-    <Nav></Nav>
-    <HeadSection></HeadSection>
-    </header>
+      <header className='header'>
+        <Nav></Nav>
+        <HeadSection></HeadSection>
+
+      </header>
+      <main className="main-content">
+        <BodySection></BodySection>
+      </main>
+      
     </div>
 
   );
